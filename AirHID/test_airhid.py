d564f5a7c8aaefb5a983714973dd86bd97f903b4
Predrag Gruevski <predrag.gruevski@gmail.com>
2013-11-20 20:51:36 -0500

    Initial AirHID exploit driver and tests.

---
+++ AirHID/test_airhid.py
@@ -0,0 +1,33 @@
+import sys
+import airhid_remote as remote
+
+IP = "192.168.107.129"
+PORT = 13246
+
+"""
+Tests the AirHID exploit driver by:
+    - opening the Windows start menu
+    - opening Yahoo in the primary web browser
+
+The tests are meant to be run on Windows 7.
+
+When running the test, you can specify the IP address of the system to be
+exploited as the first command-line parameter.
+"""
+
+def main():
+    if len(sys.argv) >= 2:
+        IP = sys.argv[1]
+
+    r = remote.AirHID_Remote(IP, PORT)
+    test_open_start_menu(r)
+    test_open_url(r)
+
+def test_open_start_menu(r):
+    r.move_mouse(-2000,2000).left_click()
+
+def test_open_url(r):
+    r.open_url("http://www.yahoo.com/")
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -2,3 +2,10 @@ pyre
 ====
 
 6.858 final project -- Python exploitation of PC-remote-control mobile apps
+
+
+Style guide
+-----------
+
+* Test files will always start with `test_`.
+* Test files should always be directly runnable from the command line and have a main function.

66c3b9352fdf26509be7114c359ef670b1650546
Albert Wu <albertyfwu@gmail.com>
2013-12-07 22:36:57 -0500

    new driver

--- MobileMouseLite/mmlite_driver.py
+++ MobileMouseLite/mmlite_driver.py
@@ -6,12 +6,49 @@ END = chr(0x04)
 
 class MMLiteDriver:
 
+    TAB = "TAB"
+    ENTER = "ENTER"
+    BACK_SPACE = "BACKSPACE"
+
+    ACTION_KEYS = {
+        'TAB': TAB,
+        'ENTER': ENTER,
+        'ESCAPE': "ESCAPE",
+        'PAGE_UP': "PGUP",
+        'PAGE_DOWN': "PGDN",
+        'END': "END",
+        'HOME': "HOME",
+        'LEFT': "LEFT",
+        'UP': "UP",
+        'RIGHT': "RIGHT",
+        'DOWN': "DOWN",
+        'BACK_SPACE': BACK_SPACE,
+        'F1': "F1",
+        'F2': "F2",
+        'F3': "F3",
+        'F4': "F4",
+        'F5': "F5",
+        'F6': "F6",
+        'F7': "F7",
+        'F8': "F8",
+        'F9': "F9",
+        'F10': "F10",
+        'F11': "F11",
+        'F12': "F12"
+    }
+
+    TRANSLATION_TABLE = {
+        '\n': ENTER,
+        '\b': BACK_SPACE,
+        '\t': TAB
+    }
+
     def __init__(self, ip, port):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.connect((ip, port))
 
     def connect(self):
-        # connect_hex says something along the lines of "CONNECTANDROIDANDROID2.0.6"
+        # connect_hex says something along the lines of "CONNECT[SEP][SEP]ANDROID[SEP]ANDROID[SEP]2.0.6[SEP]0[END]"
         connect_hex = [0x43, 0x4f, 0x4e, 0x4e, 0x45, 0x43, 0x54, 0x1e, 0x1e, 0x41, 0x6e, 0x64, 0x72, 0x6f, 0x69, 0x64, 0x1e, 0x41, 0x6e, 0x64, 0x72, 0x6f, 0x69, 0x64, 0x1e, 0x32, 0x2e, 0x30, 0x2e, 0x36, 0x1e, 0x30, 0x04]
         s = ''
         for num in connect_hex:
@@ -65,12 +102,18 @@ class MMLiteDriver:
 
     def type(self, text):
         for c in text:
-            if c == '\n':
-                c = 'ENTER'
+            if c in MMLiteDriver.TRANSLATION_TABLE:
+                c = MMLiteDriver.TRANSLATION_TABLE[c]
             self._send(SEP.join(["KEY", "-1", c, END]))
         return self
 
-    def press_action_key(self, key, shift=False, ctrl=False, alt=False):
+    def press_action_key(self, name, shift=False, ctrl=False, alt=False):
+        code = None
+        if name in MMLiteDriver.ACTION_KEYS:
+            code = MMLiteDriver.ACTION_KEYS[name]
+        else:
+            code = name
+
         keys = []
         if shift:
             keys.append('SHIFT')
@@ -80,6 +123,6 @@ class MMLiteDriver:
             keys.append('ALT')
         keys_str = '+'.join(keys)
 
-        self._send(SEP.join(["KEY", "-1", key, keys_str, END]))
+        self._send(SEP.join(["KEY", "-1", code, keys_str, END]))
         
         return self
